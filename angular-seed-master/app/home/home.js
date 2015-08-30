118e1998e57f68b7af18fd5b57f753420c57f389
usik <usik.o2@gmail.com>
2015-08-30 23:19:33 +0900

    Adding Sign in Functions
    Adding sign in functions

--- angular-seed-master/app/home/home.js
+++ angular-seed-master/app/home/home.js
@@ -1,6 +1,6 @@
 'use strict'
 
-angular.module('myApp.home',['ngRoute'])
+angular.module('myApp.home',['ngRoute','firebase'])
 
 .config(['$routeProvider',function($routeProvider){
 	$routeProvider.when('/home',{
@@ -9,6 +9,28 @@ angular.module('myApp.home',['ngRoute'])
 	});
 }])
 
-.controller('HomeCtrl',[function(){
+.controller('HomeCtrl',['$scope','$firebaseSimplelogin', function($scope, $firebaseSimplelogin){
 
+	var firebaseObj= new Firebase("https://amber-torch-5013.firebaseio.com/");
+	var loginObj = $firebaseSimplelogin(firebaseObj);
+
+	$scope.SignIn=function(event){
+		event.preventDefault(); // Preventing from refreshing
+		var username=$scope.user.email;
+		var password=$scope.user.password;
+
+		//Auth logic
+
+		loginObj.$login('password', {
+			email:username,
+			password: password
+		})
+		.then(function(user){
+			//Successful
+			console.log('Authentication successful');
+		}, function(error){
+			//Failure
+			console.log('Authentication failure');
+		});
+	}
 }]);
