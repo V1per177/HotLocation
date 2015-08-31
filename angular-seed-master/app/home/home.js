2b510c679acd7647e6cd769c24e0ff5275cc40cb
usik <usik.o2@gmail.com>
2015-08-31 16:04:06 +0900

    Implemented after sign-in page.
    Things left to do:
    Implement better UI
    Implement google maps
    Implement filters/ pickers

--- angular-seed-master/app/home/home.js
+++ angular-seed-master/app/home/home.js
@@ -1,36 +1,53 @@
-'use strict'
+'use strict';
 
 angular.module('myApp.home',['ngRoute','firebase'])
 
 .config(['$routeProvider',function($routeProvider){
 	$routeProvider.when('/home',{
-		templateUrl:'home/home.html',
+		templateUrl: 'home/home.html',
 		controller: 'HomeCtrl'
 	});
 }])
 
-.controller('HomeCtrl',['$scope','$firebaseAuth', function($scope, $firebaseAuth){
-
-	var firebaseObj= new Firebase("https://amber-torch-5013.firebaseio.com");
+.controller('HomeCtrl',['$scope','$location','CommonProp','$firebaseAuth', function($scope, $location, CommonProp, $firebaseAuth){
+	
+	var firebaseObj = new Firebase("https://amber-torch-5013.firebaseio.com");
 	var loginObj = $firebaseAuth(firebaseObj);
+	$scope.user={};
 
 	$scope.SignIn=function(e){
 		e.preventDefault(); // Preventing from refreshing
-		var username=$scope.user.email;
-		var password=$scope.user.password;
+		var username = $scope.user.email;
+		var password = $scope.user.password;
 
 		//Auth logic
+		console.log('in function');
 
 		loginObj.$authWithPassword({
-			email:username,
-			password:password
+			email: username,
+			password: password
 		})
 		.then(function(user){
 			//success login
 			console.log('Authentication successful');
+			CommonProp.setUser(user.password.email);
+			$location.path('/welcome');
+
 		},function(error){
 			//login failure
 			console.log('Authentication failure');
 		});
 	}
-}]);
+}])
+.service('CommonProp', function(){
+	var user='';
+
+	return{
+		getUser: function(){
+			return user;
+		},
+		setUser: function(value){
+			user=value;
+		}
+	};
+});
--- angular-seed-master/app/register/register.js
+++ angular-seed-master/app/register/register.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 angular.module('myApp.register', ['ngRoute','firebase'])
 
