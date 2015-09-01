753267da3f1be1914117da485048bb9a9a9503f6
usik <usik.o2@gmail.com>
2015-09-01 19:57:07 +0900

    added submitting function

--- angular-seed-master/app/addPost/addPost.js
+++ angular-seed-master/app/addPost/addPost.js
@@ -8,6 +8,29 @@ angular.module('myApp.addPost',['ngRoute'])
 	});
 }])
 
-.controller('AddPostCtrl',['$scope', function($routeProvider){
+.controller('AddPostCtrl',['$scope', '$firebase',function($scope,$firebase){
+	
+	//Instantiate firebase object
+	var firebaseObj = new Firebase("https://amber-torch-5013.firebaseio.com");
+	var fb = $firebase(firebaseObj);
+
+
+	$scope.AddPost=function(){
+		//Add Post logic
+		//get title and post
+		var title = $scope.article.title;
+		var post = $scope.article.post;
+		
+		//Call pushAPI
+		fb.$push({
+			title:title,
+			post:post
+		}).then(function(ref){
+			console.log(ref);
+		}, function(error){
+			console.log("Error:", error);
+		});
+	
+	}
 
 }]);
