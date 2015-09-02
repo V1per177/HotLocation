a055d1d550b8ec08aac33b510ef182f2d60e4d62
usik <usik.o2@gmail.com>
2015-09-02 11:57:04 +0900

    changing add post
    still not able to post

--- angular-seed-master/app/addPost/addPost.js
+++ angular-seed-master/app/addPost/addPost.js
@@ -3,28 +3,27 @@
 angular.module('myApp.addPost',['ngRoute'])
 .config(['$routeProvider', function($routeProvider){
 	$routeProvider.when('/addPost',{
-		templateUrl: 'addPost/addPost',
+		templateUrl: 'addPost/addPost.html',
 		controller:'AddPostCtrl'
 	});
 }])
 
-.controller('AddPostCtrl',['$scope', '$firebase',function($scope,$firebase){
-	
+.controller('AddPostCtrl',['$scope','$firebase',function($scope,$firebase){
+
+	$scope.AddPost=function(){
+	//get title and post
+	var title = $scope.article.title;
+	var post = $scope.article.post;
+
 	//Instantiate firebase object
 	var firebaseObj = new Firebase("https://amber-torch-5013.firebaseio.com");
 	var fb = $firebase(firebaseObj);
-
-
-	$scope.AddPost=function(){
 		//Add Post logic
-		//get title and post
-		var title = $scope.article.title;
-		var post = $scope.article.post;
-		
+
 		//Call pushAPI
 		fb.$push({
-			title:title,
-			post:post
+			title: title,
+			post: post
 		}).then(function(ref){
 			console.log(ref);
 		}, function(error){
